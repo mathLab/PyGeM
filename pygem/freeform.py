93f12e0c74eedd37f21888af03cd6e9eb6537ecb
Stefano Chiapedi <stefano.chiapedi@tum.de>
2016-08-06 11:27:07 +0200

    improved performance

--- pygem/freeform.py
+++ pygem/freeform.py
@@ -44,116 +44,125 @@ import pygem.affine as at
 
 
 class FFD(object):
-	"""
-	Class that handles the Free Form Deformation on the mesh points.
-
-	:param FFDParameters ffd_parameters: parameters of the Free Form Deformation.
-	:param numpy.ndarray original_mesh_points: coordinates of the original points of the mesh.
-
-	:cvar FFDParameters parameters: parameters of the Free Form Deformation.
-	:cvar numpy.ndarray original_mesh_points: coordinates of the original points of the mesh.
-		The shape is `n_points`-by-3.
-	:cvar numpy.ndarray modified_mesh_points: coordinates of the points of the deformed mesh.
-		The shape is `n_points`-by-3.
-
-	:Example:
-
-	>>> import pygem.freeform as ffd
-	>>> import pygem.params as ffdp
-	>>> import numpy as np
-
-	>>> ffd_parameters = ffdp.FFDParameters()
-	>>> ffd_parameters.read_parameters('tests/test_datasets/parameters_test_ffd_sphere.prm')
-	>>> original_mesh_points = np.load('tests/test_datasets/meshpoints_sphere_orig.npy')
-	>>> free_form = ffd.FFD(ffd_parameters, original_mesh_points)
-	>>> free_form.perform()
-	>>> new_mesh_points = free_form.modified_mesh_points		
     """
-	def __init__(self, ffd_parameters, original_mesh_points):
-		self.parameters = ffd_parameters
-		self.original_mesh_points = original_mesh_points
-		self.modified_mesh_points = None
+    Class that handles the Free Form Deformation on the mesh points.
+
+    :param FFDParameters ffd_parameters: parameters of the Free Form Deformation.
+    :param numpy.ndarray original_mesh_points: coordinates of the original points of the mesh.
+
+    :cvar FFDParameters parameters: parameters of the Free Form Deformation.
+    :cvar numpy.ndarray original_mesh_points: coordinates of the original points of the mesh.
+		The shape is `n_points`-by-3.
+    :cvar numpy.ndarray modified_mesh_points: coordinates of the points of the deformed mesh.
+    The shape is `n_points`-by-3.
+
+    :Example:
+
+    >>> import pygem.freeform as ffd
+    >>> import pygem.params as ffdp
+    >>> import numpy as np
+
+    >>> ffd_parameters = ffdp.FFDParameters()
+    >>> ffd_parameters.read_parameters('tests/test_datasets/parameters_test_ffd_sphere.prm')
+    >>> original_mesh_points = np.load('tests/test_datasets/meshpoints_sphere_orig.npy')
+    >>> free_form = ffd.FFD(ffd_parameters, original_mesh_points)
+    >>> free_form.perform()
+    >>> new_mesh_points = free_form.modified_mesh_points		
+    """
+    def __init__(self, ffd_parameters, original_mesh_points):
+        self.parameters = ffd_parameters
+        self.original_mesh_points = original_mesh_points
+        self.modified_mesh_points = None
 
 
-	def perform(self):
-		"""
-		This method performs the deformation on the mesh points. After the execution
-		it sets `self.modified_mesh_points`.
+    def perform(self):
+       """
+       This method performs the deformation on the mesh points. After the execution
+       it sets `self.modified_mesh_points`.
 
-		.. todo::
-			In order to improve the performances, we need to perform the FFD only on the points inside
-			the lattice.
-		"""
-		(n_rows_mesh, n_cols_mesh) = self.original_mesh_points.shape
+       .. todo::
+#		In order to improve the performances, we need to perform the FFD only on the points inside
+#		the lattice.
+       """
+       # translation and then affine transformation
+       translation = self.parameters.origin_box
+       
+       physical_frame = np.array([self.parameters.position_vertex_1 - translation, \
+                                  self.parameters.position_vertex_2 - translation, \
+                                  self.parameters.position_vertex_3 - translation, \
+                                  [0, 0, 0]])
+       reference_frame = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]])
+       
+       transformation = at.affine_points_fit(physical_frame, reference_frame)
+       inverse_transformation = at.affine_points_fit(reference_frame, physical_frame)
+       
+       box_position_x = sorted([self.parameters.origin_box[0], self.parameters.origin_box[0]+self.parameters.lenght_box_x], key=float)
+       box_position_y = sorted([self.parameters.origin_box[1], self.parameters.origin_box[1]+self.parameters.lenght_box_y], key=float)
+       box_position_z = sorted([self.parameters.origin_box[2], self.parameters.origin_box[2]+self.parameters.lenght_box_z], key=float)
+       mesh_points = self.original_mesh_points[(self.original_mesh_points[:,0] >= box_position_x[0]) & \
+                                               (self.original_mesh_points[:,0] <= box_position_x[1]) & \
+                                               (self.original_mesh_points[:,1] >= box_position_y[0]) & \
+                                               (self.original_mesh_points[:,1] <= box_position_y[1]) & \
+                                               (self.original_mesh_points[:,2] >= box_position_z[0]) & \
+                                               (self.original_mesh_points[:,2] <= box_position_z[1])]
+       (n_rows_mesh, n_cols_mesh) = mesh_points.shape
+    
+       # Initialization. In order to exploit the contiguity in memory the following are transposed
+       (dim_n_mu, dim_m_mu, dim_t_mu) = self.parameters.array_mu_x.shape
+       bernstein_x = np.zeros((dim_n_mu, n_rows_mesh))
+       bernstein_y = np.zeros((dim_m_mu, n_rows_mesh))
+       bernstein_z = np.zeros((dim_t_mu, n_rows_mesh))
+       shift_mesh_points = np.zeros((n_cols_mesh, n_rows_mesh))
+    
+       reference_frame_mesh_points = self._transform_points(mesh_points - translation, transformation)
+    
+       for i in range(0, dim_n_mu):
+           aux1 = np.power((1-reference_frame_mesh_points[:, 0]), dim_n_mu-1-i)
+           aux2 = np.power(reference_frame_mesh_points[:, 0], i)
+           bernstein_x[i, :] = special.binom(dim_n_mu-1, i) * np.multiply(aux1, aux2)
+            
+       for i in range(0, dim_m_mu):
+           aux1 = np.power((1-reference_frame_mesh_points[:, 1]), dim_m_mu-1-i)
+           aux2 = np.power(reference_frame_mesh_points[:, 1], i)
+           bernstein_y[i, :] = special.binom(dim_m_mu-1, i) * np.multiply(aux1, aux2)
+    
+       for i in range(0, dim_t_mu):
+           aux1 = np.power((1-reference_frame_mesh_points[:, 2]), dim_t_mu-1-i)
+           aux2 = np.power(reference_frame_mesh_points[:, 2], i)
+           bernstein_z[i, :] = special.binom(dim_t_mu-1, i) * np.multiply(aux1, aux2)
+    
+    
+       aux_x = 0.
+       aux_y = 0.
+       aux_z = 0.
+       for j in range(0, dim_m_mu):
+           for k in range(0, dim_t_mu):
+               bernstein_yz = np.multiply(bernstein_y[j, :], bernstein_z[k, :])
+               for i in range(0, dim_n_mu):
+                   aux = np.multiply(bernstein_x[i, :], bernstein_yz)
+                   aux_x += aux * self.parameters.array_mu_x[i, j, k]
+                   aux_y += aux * self.parameters.array_mu_y[i, j, k]
+                   aux_z += aux * self.parameters.array_mu_z[i, j, k]
+       shift_mesh_points[0, :] += aux_x
+       shift_mesh_points[1, :] += aux_y
+       shift_mesh_points[2, :] += aux_z
+    
+       # Here shift_mesh_points needs to be transposed to be summed with reference_frame_mesh_points
+       new_mesh_points = self._transform_points(np.transpose(shift_mesh_points) + reference_frame_mesh_points, \
+                                                inverse_transformation) + translation
 
-		# translation and then affine transformation
-		translation = self.parameters.origin_box
+       self.modified_mesh_points = np.copy(self.original_mesh_points)        
+       self.modified_mesh_points[(self.original_mesh_points[:,0] >= box_position_x[0]) & \
+                                 (self.original_mesh_points[:,0] <= box_position_x[1]) & \
+                                 (self.original_mesh_points[:,1] >= box_position_y[0]) & \
+                                 (self.original_mesh_points[:,1] <= box_position_y[1]) & \
+                                 (self.original_mesh_points[:,2] >= box_position_z[0]) & \
+                                 (self.original_mesh_points[:,2] <= box_position_z[1])] \
+                                 = new_mesh_points
 
-		fisical_frame = np.array([self.parameters.position_vertex_1 - translation, \
-			  self.parameters.position_vertex_2 - translation, \
-			  self.parameters.position_vertex_3 - translation, \
-			  [0, 0, 0]])
-		reference_frame = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]])
-
-		transformation = at.affine_points_fit(fisical_frame, reference_frame)
-		inverse_transformation = at.affine_points_fit(reference_frame, fisical_frame)
-
-		# Initialization. In order to exploit the contiguity in memory the following are transposed
-		(dim_n_mu, dim_m_mu, dim_t_mu) = self.parameters.array_mu_x.shape
-		bernstein_x = np.zeros((dim_n_mu, n_rows_mesh))
-		bernstein_y = np.zeros((dim_m_mu, n_rows_mesh))
-		bernstein_z = np.zeros((dim_t_mu, n_rows_mesh))
-		shift_mesh_points = np.zeros((n_cols_mesh, n_rows_mesh))
-
-		reference_frame_mesh_points = self._transform_points(self.original_mesh_points - translation, \
-															 transformation)
-
-		for i in range(0, dim_n_mu):
-			aux1 = np.power((1-reference_frame_mesh_points[:, 0]), dim_n_mu-1-i)
-			aux2 = np.power(reference_frame_mesh_points[:, 0], i)
-			bernstein_x[i, :] = special.binom(dim_n_mu-1, i) * np.multiply(aux1, aux2)
-
-		for i in range(0, dim_m_mu):
-			aux1 = np.power((1-reference_frame_mesh_points[:, 1]), dim_m_mu-1-i)
-			aux2 = np.power(reference_frame_mesh_points[:, 1], i)
-			bernstein_y[i, :] = special.binom(dim_m_mu-1, i) * np.multiply(aux1, aux2)
-
-		for i in range(0, dim_t_mu):
-			aux1 = np.power((1-reference_frame_mesh_points[:, 2]), dim_t_mu-1-i)
-			aux2 = np.power(reference_frame_mesh_points[:, 2], i)
-			bernstein_z[i, :] = special.binom(dim_t_mu-1, i) * np.multiply(aux1, aux2)
-
-
-		aux_x = 0.
-		aux_y = 0.
-		aux_z = 0.
-		for j in range(0, dim_m_mu):
-			for k in range(0, dim_t_mu):
-				bernstein_yz = np.multiply(bernstein_y[j, :], bernstein_z[k, :])
-				for i in range(0, dim_n_mu):
-					aux = np.multiply(bernstein_x[i, :], bernstein_yz)
-					aux_x += aux * self.parameters.array_mu_x[i, j, k]
-					aux_y += aux * self.parameters.array_mu_y[i, j, k]
-					aux_z += aux * self.parameters.array_mu_z[i, j, k]
-		shift_mesh_points[0, :] += aux_x
-		shift_mesh_points[1, :] += aux_y
-		shift_mesh_points[2, :] += aux_z
-
-		# Splitting points inside and outside the lattice: TODO not very efficient
-		for i in range(0, n_rows_mesh):
-			if (reference_frame_mesh_points[i, 0] < 0) or (reference_frame_mesh_points[i, 1] < 0) \
-			or (reference_frame_mesh_points[i, 2] < 0) or (reference_frame_mesh_points[i, 0] > 1) \
-			or (reference_frame_mesh_points[i, 1] > 1) or (reference_frame_mesh_points[i, 2] > 1):
-				shift_mesh_points[:, i] = 0
-
-		# Here shift_mesh_points needs to be transposed to be summed with reference_frame_mesh_points
-		self.modified_mesh_points = self._transform_points(np.transpose(shift_mesh_points) + \
-									reference_frame_mesh_points, inverse_transformation) + translation
-
-
-	@staticmethod
-	def _transform_points(original_points, transformation):
-		"""
+    @staticmethod
+    def _transform_points(original_points, transformation):
+        """
 		This private static method transforms the points according to the affine transformation taken from
 		affine_points_fit method.
 
@@ -162,11 +171,10 @@ class FFD(object):
 
 		:return: modified_points: coordinates of the modified points.
 		:rtype: numpy.ndarray
-		"""
-		n_rows_mesh, n_cols_mesh = original_points.shape
-		modified_points = np.zeros((n_rows_mesh, n_cols_mesh))
-
-		for i in range(0, n_rows_mesh):
-			modified_points[i, :] = transformation(original_points[i])
-
-		return modified_points
+        """
+        n_rows_mesh, n_cols_mesh = original_points.shape
+        modified_points = np.zeros((n_rows_mesh, n_cols_mesh))
+        for i in range(0, n_rows_mesh):
+            modified_points[i, :] = transformation(original_points[i])
+          
+        return modified_points
