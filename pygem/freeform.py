fixed error with box rotation

pygem/freeform.py
@@ -86,24 +86,25 @@ class FFD(object):
 		# translation and then affine transformation
 		translation = self.parameters.origin_box
 
-		physical_frame = np.array([self.parameters.position_vertex_1 - translation, \
-								   self.parameters.position_vertex_2 - translation, \
-								   self.parameters.position_vertex_3 - translation, \
+		physical_frame = np.array([self.parameters.position_vertex_1 - translation,
+								   self.parameters.position_vertex_2 - translation,
+								   self.parameters.position_vertex_3 - translation,
 								   [0, 0, 0]])
 		reference_frame = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]])
 
 		transformation = at.affine_points_fit(physical_frame, reference_frame)
 		inverse_transformation = at.affine_points_fit(reference_frame, physical_frame)
-	   
-		box_position_x = sorted([self.parameters.origin_box[0], self.parameters.origin_box[0]+self.parameters.lenght_box_x], key=float)
-		box_position_y = sorted([self.parameters.origin_box[1], self.parameters.origin_box[1]+self.parameters.lenght_box_y], key=float)
-		box_position_z = sorted([self.parameters.origin_box[2], self.parameters.origin_box[2]+self.parameters.lenght_box_z], key=float)
-		mesh_points = self.original_mesh_points[(self.original_mesh_points[:,0] >= box_position_x[0]) & \
-												(self.original_mesh_points[:,0] <= box_position_x[1]) & \
-												(self.original_mesh_points[:,1] >= box_position_y[0]) & \
-												(self.original_mesh_points[:,1] <= box_position_y[1]) & \
-												(self.original_mesh_points[:,2] >= box_position_z[0]) & \
-												(self.original_mesh_points[:,2] <= box_position_z[1])]
+
+		# apply transformation to original mesh points
+		reference_frame_mesh_points = self._transform_points(self.original_mesh_points - translation, transformation)
+
+		# select mesh points inside bounding box
+		mesh_points = reference_frame_mesh_points[(reference_frame_mesh_points[:,0] >= 0.) &
+												  (reference_frame_mesh_points[:,0] <= 1.) &
+												  (reference_frame_mesh_points[:,1] >= 0.) &
+												  (reference_frame_mesh_points[:,1] <= 1.) &
+												  (reference_frame_mesh_points[:,2] >= 0.) &
+												  (reference_frame_mesh_points[:,2] <= 1.)]
 		(n_rows_mesh, n_cols_mesh) = mesh_points.shape
 
 		# Initialization. In order to exploit the contiguity in memory the following are transposed
@@ -113,21 +114,19 @@ class FFD(object):
 		bernstein_z = np.zeros((dim_t_mu, n_rows_mesh))
 		shift_mesh_points = np.zeros((n_cols_mesh, n_rows_mesh))
 
-		reference_frame_mesh_points = self._transform_points(mesh_points - translation, transformation)
-
 		for i in range(0, dim_n_mu):
-			aux1 = np.power((1-reference_frame_mesh_points[:, 0]), dim_n_mu-1-i)
-			aux2 = np.power(reference_frame_mesh_points[:, 0], i)
+			aux1 = np.power((1-mesh_points[:, 0]), dim_n_mu-1-i)
+			aux2 = np.power(mesh_points[:, 0], i)
 			bernstein_x[i, :] = special.binom(dim_n_mu-1, i) * np.multiply(aux1, aux2)
 
 		for i in range(0, dim_m_mu):
-			aux1 = np.power((1-reference_frame_mesh_points[:, 1]), dim_m_mu-1-i)
-			aux2 = np.power(reference_frame_mesh_points[:, 1], i)
+			aux1 = np.power((1-mesh_points[:, 1]), dim_m_mu-1-i)
+			aux2 = np.power(mesh_points[:, 1], i)
 			bernstein_y[i, :] = special.binom(dim_m_mu-1, i) * np.multiply(aux1, aux2)
 
 		for i in range(0, dim_t_mu):
-			aux1 = np.power((1-reference_frame_mesh_points[:, 2]), dim_t_mu-1-i)
-			aux2 = np.power(reference_frame_mesh_points[:, 2], i)
+			aux1 = np.power((1-mesh_points[:, 2]), dim_t_mu-1-i)
+			aux2 = np.power(mesh_points[:, 2], i)
 			bernstein_z[i, :] = special.binom(dim_t_mu-1, i) * np.multiply(aux1, aux2)
 
 
@@ -146,18 +145,21 @@ class FFD(object):
 		shift_mesh_points[1, :] += aux_y
 		shift_mesh_points[2, :] += aux_z
 	
-		# Here shift_mesh_points needs to be transposed to be summed with reference_frame_mesh_points
-		new_mesh_points = self._transform_points(np.transpose(shift_mesh_points) + reference_frame_mesh_points, \
-												 inverse_transformation) + translation
+		# shift_mesh_points needs to be transposed to be summed with mesh_points
+		# apply inverse transformation to shifted mesh points
+		new_mesh_points = self._transform_points(np.transpose(shift_mesh_points) +
+												 mesh_points, inverse_transformation) + \
+						  translation
 
-		self.modified_mesh_points = np.copy(self.original_mesh_points)		
-		self.modified_mesh_points[(self.original_mesh_points[:,0] >= box_position_x[0]) & \
-								  (self.original_mesh_points[:,0] <= box_position_x[1]) & \
-								  (self.original_mesh_points[:,1] >= box_position_y[0]) & \
-								  (self.original_mesh_points[:,1] <= box_position_y[1]) & \
-								  (self.original_mesh_points[:,2] >= box_position_z[0]) & \
-								  (self.original_mesh_points[:,2] <= box_position_z[1])] \
-								  = new_mesh_points
+		# merge non-shifted mesh points with shifted ones
+		self.modified_mesh_points = np.copy(self.original_mesh_points)
+		self.modified_mesh_points[(reference_frame_mesh_points[:,0] >= 0.) &
+								  (reference_frame_mesh_points[:,0] <= 1.) &
+								  (reference_frame_mesh_points[:,1] >= 0.) &
+								  (reference_frame_mesh_points[:,1] <= 1.) &
+								  (reference_frame_mesh_points[:,2] >= 0.) &
+								  (reference_frame_mesh_points[:,2] <= 1.)] \
+			= new_mesh_points
 
 	@staticmethod
 	def _transform_points(original_points, transformation):